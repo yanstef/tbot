9adb24a3b15c567fe7c660aa105cbb95065ec497
yanstef <yanickstephane@live.com>
2025-02-12 02:05:32 +0100

    test push

--- src/main.py
+++ src/main.py
@@ -5,9 +5,11 @@ from dotenv import load_dotenv
 from command import show_help, get_input
 
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), "../.env")
 print(dotenv_path)
 load_dotenv(dotenv_path)
+
+# test
 
 
 def main():
@@ -15,13 +17,20 @@ def main():
     Handles the initial launch of the program (entry point).
     """
     token = os.getenv("BOT_TOKEN")
-    application = Application.builder().token(token).concurrent_updates(True).read_timeout(30).write_timeout(30).build() # noqa
-    application.add_handler(CommandHandler('start', show_help))
-    application.add_handler(CommandHandler('help', show_help))
+    application = (
+        Application.builder()
+        .token(token)
+        .concurrent_updates(True)
+        .read_timeout(30)
+        .write_timeout(30)
+        .build()
+    )  # noqa
+    application.add_handler(CommandHandler("start", show_help))
+    application.add_handler(CommandHandler("help", show_help))
     application.add_handler(MessageHandler(filters.TEXT, get_input))
     print("TeleQR instance started!")
     application.run_polling()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
